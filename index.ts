7958f633d4d759ff777141ca3cb99ef197ce2994
Avdi Grimm <avdi@avdi.codes>
2019-06-03 18:04:21 -0500

    ~you can type and quit

--- index.ts
+++ index.ts
@@ -1,18 +1,28 @@
 #!/usr/bin/node
 import readline from "readline-promise";
+import { promisify } from "util";
 const prompt = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: true
 });
 
-welcomeThePlayer();
-echoACommand(prompt);
+main();
 
-async function echoACommand(prompt: any) {
-    const command: string = await prompt.questionAsync("> ");
-    console.log(command);
-    prompt.close();
+function main() {
+    welcomeThePlayer();
+    melvin(prompt);
+}
+
+async function melvin(prompt: any) {
+    while (true) {
+        const command: string = await prompt.questionAsync("> ");
+        if ("quit" === command) {
+            prompt.close();
+            return;
+        }
+        console.log(command);
+    }
 }
 
 function welcomeThePlayer() {
