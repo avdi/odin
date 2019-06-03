e3a8c3abe8b91721a88d5db7a05b4d8c34a7f5a1
Avdi Grimm <avdi@avdi.codes>
2019-06-03 17:36:54 -0500

    Single echo with readline and promises

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
 #!/usr/bin/node
+import * as readline from "readline";
+const prompt = readline.createInterface({ input: process.stdin, output: process.stdout });
+
 welcomeThePlayer();
+const p = new Promise((resolve, reject) => {
+    prompt.question("> ", answer => {
+        resolve(answer);
+    });
+});
+p.then(answer => {
+    console.log(answer);
+    prompt.close();
+})
 
 function welcomeThePlayer() {
     const theWelcomeMessage = "Welcome to the caaaaaaves of Oooooooodiiiiiinnn";
