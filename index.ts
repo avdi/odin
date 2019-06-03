f8f720990ca84623c00fdd684ab0f93dbfb3d504
Avdi Grimm <avdi@avdi.codes>
2019-06-03 18:21:28 -0500

    Refactor

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
 #!/usr/bin/node
 import readline from "readline-promise";
 import { promisify } from "util";
-const prompt = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: true
-});
+
 
 main();
 
-function main() {
+async function main() {
+    const prompt = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: true
+    });
     welcomeThePlayer();
-    melvin(prompt);
+    await enterCommmandLoop(prompt);
+    prompt.close();
 }
 
 const getOut = ["quit", "exit", "go away", "fuck off"];
 
-async function melvin(prompt: any) {
+async function enterCommmandLoop(prompt: any) {
     while (true) {
         const command: string = await prompt.questionAsync("> ");
         if (getOut.includes(command)) {
-            prompt.close();
             return;
         }
         console.log(command);
